cd823331f64ffc69ff5c782dbed176987edd82ed
weffington <noah.hampton03@gmail.com>
2025-02-13 11:48:19 -0700

    message for hw1p4

---
+++ Homework/Homework2/hw2p1.sh
@@ -0,0 +1,21 @@
+#Noah Hampton
+#Homework 2 Problem 1
+#4a)
+
+git status
+
+#if branch is up to date:
+
+git pull origin main
+
+#push protocol
+
+git status
+git add <filename.py>
+git commit -m "commit message"
+git push origin main
+
+#4b)
+#remove git repo
+rm -rf .git
+rm -rf .gitignore
