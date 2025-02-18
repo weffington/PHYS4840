c85ad125cdbb37aa36ebf1507da0718736ea4fee
weffington <noah.hampton03@gmail.com>
2025-02-18 16:58:49 -0700

    compiled homework 2

---
+++ Homework/Homework2/Compiledhw2.py
@@ -0,0 +1,158 @@
+#Homework 2
+#Noah Hampton
+#!/usr/bin/env python3.10.12
+
+#Problem 1
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import my_functions_lib as mfl
+
+
+'''
+This code block loads and cleans the theoretical models from
+the iso.cmd file. It is the same code as in isochrone_in-class_demo.py
+but with the comments and instructions removed. Consult the commented 
+version of this file if you are confused about any of these steps 
+'''
+load_file = 'MIST_v1.2_feh_m1.75_afe_p0.0_vvcrit0.4_HST_WFPC2.iso.cmd'
+
+log10_isochrone_age_yr, F606, F814,\
+logL, logTeff, phase= np.loadtxt(load_file, usecols=(1,14,18,6,4,22), unpack=True, skiprows=14)
+
+age_Gyr_1e9 = (10.0**log10_isochrone_age_yr)/1e9
+age_Gyr = age_Gyr_1e9
+
+age_selection = np.where((age_Gyr > 12) & (age_Gyr <= 13.8)) 
+
+color_selected = F606[age_selection]-F814[age_selection]
+magnitude_selected = F606[age_selection]
+
+Teff = 10.0**logTeff
+Teff_for_desired_ages =  Teff[age_selection]
+logL_for_desired_ages =  logL[age_selection]
+
+phases_for_desired_age = phase[age_selection]
+desired_phases = np.where(phases_for_desired_age <= 3)
+
+## now, we can restrict our equal-sized arrays by phase
+cleaned_color = color_selected[desired_phases]
+cleaned_magnitude = magnitude_selected[desired_phases]
+cleaned_Teff = Teff_for_desired_ages[desired_phases]
+cleaned_logL = logL_for_desired_ages[desired_phases]
+
+####################################################################
+
+'''
+Now, recall how we loaded, cleaned, and plotted NGC6341.dat
+'''
+filename = 'NGC6341.dat'
+
+## # Col.  9: F336W calibrated magnitude
+## # Col. 15: F438W calibrated magnitude
+## # Col. 27: F814W calibrated magnitude
+## # Col. 33: membership probability
+## but Python indexes from 0, not 1!
+
+blue, green, red, probability = np.loadtxt(filename, usecols=(8, 14, 26, 32), unpack=True)
+mu=mfl.dmodulus(8630)
+magnitude=blue-mu
+color = blue - red
+
+quality_cut = np.where( (red   > -99.) &\
+					    (blue  > -99)  &\
+					    (green > -99)  &\
+					    (probability != -1))
+ 
+print("quality_cut: ", quality_cut )
+
+
+fig, ax = plt.subplots(figsize=(8,16))
+
+plt.plot(color[quality_cut], magnitude[quality_cut], "k.", markersize = 4, alpha = 0.2)
+plt.plot(cleaned_color,cleaned_magnitude)
+plt.gca().invert_yaxis()
+plt.xlabel("Color: B-R", fontsize=20)
+plt.ylabel("Magnitude: B", fontsize=20)
+plt.title('Hubble Space Telescope Data for\nthe Globular Cluster NGC6341', fontsize=22)
+plt.xlim(-2, 4)
+plt.ylim(10.5,-2)
+plt.legend(['HST Data','Isochrone Model'])
+plt.show()
+plt.close()
+#from the plot, we can conclude that this isochrone model is not a good fit for our data
+
+#Problem 2
+x = np.linspace(-100,100,10000)
+y = x**4
+logy=np.log(y)
+ylog = np.log(x**4)
+
+fig, ax=plt.subplots(1,3,figsize=(15,5))
+
+ax[0].plot(x,y,'r-')
+ax[0].set_title('$y=x^4')
+ax[0].set_xlabel('x')
+ax[0].set_ylabel('y')
+ax[0].grid(True)
+
+ax[1].plot(x,y,'b-')
+ax[1].set_xscale('log')
+ax[1].set_yscale('log')
+ax[1].set_title('Log-log of $y=x^4$')
+ax[1].set_xlabel('log(x)')
+ax[1].set_ylabel('log(y)')
+ax[1].grid(True,which='both')
+
+ax[2].plot(x,logy,'g-')
+ax[2].set_title('$log_{10}(y)$')
+ax[2].set_xlabel('x')
+ax[2].set_ylabel('log(y)')
+ax[2].grid(True)
+
+plt.show()
+plt.close()
+
+#Problem 3
+#a)
+filename='sunspots.txt'
+matrix = np.loadtxt('sunspots.txt',float)
+length=len(matrix)
+x=[]
+y=[]
+for i in range(0,len(matrix)):
+    y.append(matrix[i,1])
+    x.append(matrix[i,0])
+plt.plot(x,y,'k-')
+plt.title('Number of Observed Sunspots Since January 1749')
+plt.xlabel('Number of Months Since January 1749')
+plt.ylabel('Number of Sunspots')
+plt.show()
+plt.close()
+
+#b)
+plt.plot(x[:1000],y[:1000],'k-')
+plt.title('Number of Observed Sunspots Since January 1749')
+plt.xlabel('Number of Months Since January 1749')
+plt.xlabel('Number of Months Since January 1749')
+plt.ylabel('Number of Sunspots')
+plt.show()
+plt.close()
+
+#c)
+Y=[0,0,0,0]
+for i in range(5, len(y)-4):
+    sumy=[]
+    for j in range(i-5,i+5):
+        sumy.append(y[j])
+    avg=(1/11)*sum(sumy)
+    Y.append(avg)
+plt.plot(x[:1000],y[:1000],'k-')
+plt.plot(x[5:1000],Y[5:1000],'r-')
+plt.title('Number of Observed Sunspots Since January 1749')
+plt.xlabel('Number of Months Since January 1749')
+plt.xlabel('Number of Months Since January 1749')
+plt.ylabel('Number of Sunspots')
+plt.legend(['Number of Sunspots','Running Average'])
+plt.show()
+plt.close()
